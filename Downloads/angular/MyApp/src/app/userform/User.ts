07e34b59f80b528e8f4515b82cdc6d52ee584c25
unknown <ibmjavanh09@iiht.tech>
2021-04-15 14:01:45 +0530

    css support

---
+++ Downloads/angular/MyApp/src/app/userform/User.ts
@@ -0,0 +1,5 @@
+
+export class User{
+      firstname:string='Ram';
+      age:number=0;
+}
--- Downloads/angular/MyApp/src/app/userform/userform.component.ts
+++ Downloads/angular/MyApp/src/app/userform/userform.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from './User';
+
 @Component({
   selector: 'app-userform',
   templateUrl: './userform.component.html',
@@ -7,14 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserformComponent implements OnInit {
   title:string='Userform';
-  firstname:string='Ram'
-  age:number=12;
+  user:User= new User();//model -stores from all data
+  userArray:User[]=[];
   constructor() { }
   save(){
+    this.userArray.push(Object.assign({},this.user));
     console.log('working...');
-    console.log(this.firstname);
-    this.firstname='John';
-    console.log(this.age);
+    console.log(this.user.firstname);
+    //this.user.firstname='John';
+    //this.user.age=12;
+    //console.log(this.user.age);
 
   }
   ngOnInit(): void {
